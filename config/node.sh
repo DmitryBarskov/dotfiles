cdc857533611cb619a490c63d7aa12b53f0a2c07
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-04-16 15:17:09 +0200

    Use find instead of ls

--- config/node.sh
+++ config/node.sh
@@ -5,10 +5,9 @@ export NVM_DIR="$HOME/.nvm"
 
 # Load latest node installed via NVM
 if [[ -d "$NVM_DIR/versions/node" ]]; then
-    latest_node_version=$(ls -t "$NVM_DIR/versions/node" | head -1)
-    node_path="$NVM_DIR/versions/node/$latest_node_version"
-    if [[ -n "$latest_node_version" && -d "$node_path" ]]; then
-        export PATH="$node_path/bin:$PATH"
+    latest_node_path=$(find "$NVM_DIR/versions/node" -depth 1 | sort -V | tail -1)
+    if [[ -d "$latest_node_path" ]]; then
+        export PATH="$latest_node_path/bin:$PATH"
     fi
 fi
 
