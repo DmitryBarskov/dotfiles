927d6b0733e6b32eb4bd5d1f2e863675ddcc0f23
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-04-16 11:47:29 +0200

    Support MacOS Homebrew on different architectures

--- config/git.zsh
+++ config/git.zsh
@@ -3,4 +3,5 @@ precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
 zstyle ':vcs_info:git:*' formats '%F{red}%b%f '
+# Add git branch to prompt
 export PROMPT='%F{blue}%~%f $vcs_info_msg_0_%(?.$.%F{red}$%f) '
