1e47542c165348000f2483351f928f48e2b8bee5
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-04-16 15:17:40 +0200

    Speed up NVM during shell initialization (#7)

--- config/node.sh
+++ config/node.sh
@@ -1,3 +1,15 @@
-# Load node via NVM (slow)
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Load NVM as usual (slow)
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Load latest node installed via NVM
+if [[ -d "$NVM_DIR/versions/node" ]]; then
+    latest_node_path=$(find "$NVM_DIR/versions/node" -depth 1 | sort -V | tail -1)
+    if [[ -d "$latest_node_path" ]]; then
+        export PATH="$latest_node_path/bin:$PATH"
+    fi
+fi
+
+# Lazy load NVM
+alias nvm='unalias nvm && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; nvm'
