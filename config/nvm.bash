81cb8885d9fee4903c6661c4eb29d3dc3e436ee7
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-04-19 20:11:17 +0200

    Change indentation to 2 spaces

--- config/nvm.bash
+++ config/nvm.bash
@@ -3,19 +3,19 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 
 # Load latest node installed via NVM
 if [[ -d "$NVM_DIR/versions/node" ]]; then
-    latest_node_path=$(find "$NVM_DIR/versions/node" -depth 1 | sort -V | tail -1)
-    if [[ -d "$latest_node_path" ]]; then
-        export PATH="$latest_node_path/bin:$PATH"
-    fi
+  latest_node_path=$(find "$NVM_DIR/versions/node" -depth 1 | sort -V | tail -1)
+  if [[ -d "$latest_node_path" ]]; then
+    export PATH="$latest_node_path/bin:$PATH"
+  fi
 fi
 
 # Lazy load NVM
 load_nvm () {
-    if [ -s "$NVM_DIR/nvm.sh" ]; then
-        source "$NVM_DIR/nvm.sh"
-    fi
-    if [[ -n $BASH && -s "$NVM_DIR/bash_completion" ]]; then
-        source "$NVM_DIR/bash_completion"
-    fi
+  if [ -s "$NVM_DIR/nvm.sh" ]; then
+    source "$NVM_DIR/nvm.sh"
+  fi
+  if [[ -n $BASH && -s "$NVM_DIR/bash_completion" ]]; then
+    source "$NVM_DIR/bash_completion"
+  fi
 }
 alias nvm='unalias nvm && load_nvm; nvm'
--- scripts/git-profiles.bash
+++ scripts/git-profiles.bash
@@ -4,9 +4,9 @@ profile_template="[user]\n  email = \n  signingkey = \n"
 
 # Create template for git configs
 if [ ! -s config/git/personal-profile.gitconfig ]; then
-    printf "$profile_template" > config/git/personal-profile.gitconfig
+  printf "$profile_template" > config/git/personal-profile.gitconfig
 fi
 
 if [ ! -s config/git/work-profile.gitconfig ]; then
-    printf "$profile_template" > config/git/work-profile.gitconfig
+  printf "$profile_template" > config/git/work-profile.gitconfig
 fi
