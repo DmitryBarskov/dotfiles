ecfd2e2a3a18f272006d9ffa59b7fae2c2c29331
Dmitry Barskov <>
2024-01-20 19:16:27 +0000

    Fix asdf installtion

---
+++ config/asdf/setup.sh
@@ -0,0 +1,4 @@
+asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
