27fabe8fadde19a781511379bb37795ddbd2541a
Dmitry Barskov <dmitry.barskov64@gmail.com>
2025-07-14 16:15:22 +0200

    Upd

--- config/aliases.bash
+++ config/aliases.bash
@@ -2,6 +2,8 @@ alias e='eval $EDITOR'
 alias g='git'
 alias untar='tar -zxvf'
 
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
 # Common typos
 alias gs='git status'
 alias gp='git pull'
