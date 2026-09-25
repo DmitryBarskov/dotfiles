0855d36ae6695ee883525bd76d4fc9d2652e0de1
Dmitry Barskov <dmitry.barskov64@gmail.com>
2026-09-25 10:31:34 +0200

    Simplify installation, less ~/.dotfiles

--- config/env.sh
+++ config/env.sh
@@ -16,6 +16,11 @@ export HISTFILE="$XDG_STATE_HOME/history"
 export HISTSIZE=1000
 export HISTFILESIZE=2000
 export SAVEHIST=1000
+export LESSHISTFILE="$XDG_STATE_HOME/lesshst"
+export PGHISTFILE="$XDG_STATE_HOME/psql_history"
+export NODE_REPL_HISTORY="$XDG_STATE_HOME/node_repl_history"
+export PYTHONHISTFILE="$XDG_STATE_HOME/python_history"
+export RUBY_DEBUG_HISTORY_FILE="$XDG_STATE_HOME/rdbg_history"
 
 export DOCKER_HIDE_LEGACY_COMMANDS=true
 
